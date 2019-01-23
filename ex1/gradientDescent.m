34f7e54790be8335c5be15ff8251dae2e8c88168
Akansha Gautam <akansha16221@iiitd.ac.in>
2019-01-23 22:13:04 +0530

    Solution Graded

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,10 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
+    k = 1:m;
+    t1 = sum((theta(1) + theta(2) .* X(k,2)) - y(k))
+    t2 = sum(((theta(1) + theta(2) .* X(k,2)) - y(k)) .* X(k,2));
+  
+    theta(1) = theta(1) - alpha * (1/m) * t1;
+    theta(2) = theta(2) - alpha * (1/m) * t2;
 
 
 
